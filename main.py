ff10c8418b78ff3f91b3fccf25c1f1e87e5589d3
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 08:36:30 +0000

    7lab9

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # Make your Code
 # ******************************
 names = list(map(str, input().split()))
+names = sorted(names)
 shortest = min(names, key=len)
 longest = max(names, key=len)
 print(shortest, longest)
+    
     
