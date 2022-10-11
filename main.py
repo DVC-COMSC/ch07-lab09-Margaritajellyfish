22cdfcbdfe60773929ae9f91a22e4b2f903c88ba
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 08:29:45 +0000

    7lab9

--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 # Make your Code
 # ******************************
 names = list(map(str, input().split()))
-longest = int(0)
-shortest = int(99)
-for i in range(len(names)):
-    if int(len(names[i])) > longest:
-        longest = names[i]
-    if int(len(names[i])) < shortest:
-        shortest = names[i]
+shortest = min(names, key=len)
+longest = max(names, key=len)
 print(shortest, longest)
+    
